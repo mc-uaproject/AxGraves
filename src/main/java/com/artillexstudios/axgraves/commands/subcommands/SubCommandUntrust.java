943de71904280615a546e0273bb891c8cdc334ee
Sóla Lusøt <60041069+solaluset@users.noreply.github.com>
2025-03-04 23:47:00 +0200

    Small fixes & better UUID fetching

--- src/main/java/com/artillexstudios/axgraves/commands/subcommands/SubCommandUntrust.java
+++ src/main/java/com/artillexstudios/axgraves/commands/subcommands/SubCommandUntrust.java
@@ -1,14 +1,12 @@
 package com.artillexstudios.axgraves.commands.subcommands;
 
 import com.artillexstudios.axgraves.AxGraves;
-import org.bukkit.Bukkit;
-import org.bukkit.OfflinePlayer;
+import com.artillexstudios.axgraves.utils.Utils;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 import java.util.Map;
-import java.util.UUID;
 
 import static com.artillexstudios.axgraves.AxGraves.MESSAGEUTILS;
 
@@ -20,34 +18,26 @@ public enum SubCommandUntrust {
             MESSAGEUTILS.sendLang(player, "trust.player-not-found", Map.of("%player%", "Unknown"));
             return;
         }
-        UUID targetU = getUUID(target);
-
-        if (targetU == null) {
-            MESSAGEUTILS.sendLang(player, "trust.player-not-found", Map.of("%player%", target));
-            return;
-        }
-
-        String pu = player.getUniqueId().toString();
-        String tu = targetU.toString();
-        List<String> tl = AxGraves.TRUSTED.getStringList("trusted." + pu);
-
-        if (!tl.contains(tu)) {
-            MESSAGEUTILS.sendLang(player, "trust.already-trusted", Map.of("%player%", target));
-            return;
-        }
-
-        tl.remove(tu);
-        AxGraves.TRUSTED.set("trusted." + pu, tl);
-        AxGraves.TRUSTED.save();
-        MESSAGEUTILS.sendLang(player, "trust.removed", Map.of("%player%", target));
-    }
-
-    private UUID getUUID(String name) {
-        for (OfflinePlayer player : Bukkit.getOfflinePlayers()) {
-            if (player.getName() != null && player.getName().equalsIgnoreCase(name)) {
-                return player.getUniqueId();
+        Utils.getPlayerUUID(target).thenApplyAsync(targetU -> {
+            if (targetU == null) {
+                MESSAGEUTILS.sendLang(player, "trust.player-not-found", Map.of("%player%", target));
+                return null;
             }
-        }
-        return null;
+
+            String pu = player.getUniqueId().toString();
+            String tu = targetU.toString();
+            List<String> tl = AxGraves.TRUSTED.getStringList("trusted." + pu);
+
+            if (!tl.contains(tu)) {
+                MESSAGEUTILS.sendLang(player, "trust.already-trusted", Map.of("%player%", target));
+                return null;
+            }
+
+            tl.remove(tu);
+            AxGraves.TRUSTED.set("trusted." + pu, tl);
+            AxGraves.TRUSTED.save();
+            MESSAGEUTILS.sendLang(player, "trust.removed", Map.of("%player%", target));
+            return null;
+        });
     }
 }
--- src/main/java/com/artillexstudios/axgraves/utils/Utils.java
+++ src/main/java/com/artillexstudios/axgraves/utils/Utils.java
@@ -1,10 +1,16 @@
 package com.artillexstudios.axgraves.utils;
 
+import com.janboerman.invsee.spigot.api.InvseeAPI;
+import com.janboerman.invsee.spigot.api.InvseePlusPlus;
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.SkullMeta;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
 
 public class Utils {
 
@@ -17,5 +23,15 @@ public class Utils {
         }
 
         return it;
+    }
+
+    public static CompletableFuture<UUID> getPlayerUUID(@NotNull String name) {
+        final InvseePlusPlus invseePlusPlus = (InvseePlusPlus) Bukkit.getServer().getPluginManager().getPlugin("InvSeePlusPlus");
+        if (invseePlusPlus == null) {
+            final OfflinePlayer player = Bukkit.getOfflinePlayer(name);
+            return CompletableFuture.completedFuture(player.getUniqueId());
+        }
+        final InvseeAPI invseeApi = invseePlusPlus.getApi();
+        return invseeApi.fetchUniqueId(name).thenApply(optionalUUID -> optionalUUID.orElse(null));
     }
 }
